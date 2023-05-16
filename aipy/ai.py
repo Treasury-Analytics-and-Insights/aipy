1994e22bd8b26f259dd0ab98748a4624fd926215
Cory Davis <cory.davis@treasury.govt.nz>
2023-05-16 21:41:49 +1200

    classes that keep track of messages to have real convo

--- aipy/ai.py
+++ aipy/ai.py
@@ -1,34 +1,39 @@
 import openai
 import os
 
-DEFAULT_QUESTION = 'Describe the purpose and algorithm for this code'
 DEFAULT_MODEL = 'gpt-3.5-turbo'
 
-def gptchat(messages, max_tokens):
-    completions = openai.ChatCompletion.create(
-        model=DEFAULT_MODEL, messages=messages, max_tokens=max_tokens,
-        temperature=0.2)
-    print(completions['choices'][0]['message']['content'])
-    return completions
+class Chat(object):
+    def __init__(self) -> None:
+        self.messages = messages=[
+            {"role": "system", "content": 'You are wise and helpful.'}]
+        
+    def ask(self, question, max_tokens=2000, model=DEFAULT_MODEL, temperature=1):
+        messages = self.messages + [{"role": "user", "content": question}]
+        completions = openai.ChatCompletion.create(
+            model=model, messages=messages, max_tokens=max_tokens,
+            temperature=temperature)
+        answer = completions['choices'][0]['message']['content']
+        messages.append({"role":"assistant", "content": answer})
+        self.messages = messages
+        return completions
 
 
-def describe_code(code_path, question=DEFAULT_QUESTION, max_tokens=2000):
+class CodeInspector(Chat):
     lang_map = {'py': 'Python', 'R': 'R'}
-    language = lang_map[os.path.splitext(code_path)[1][1:]]
-    with open(code_path) as file:
-        code = file.read()
-    messages=[
-        {"role": "system", "content": f'You are an expert in the {language} language.'},
-        {"role": "user", "content": f"{question}: {code}"},
-    ]
-    completions = gptchat(messages, max_tokens)
-    return completions
+    default_question = 'Describe the purpose and algorithm for this code'
 
+    def __init__(self, code_path) -> None:
+        language = lang_map[os.path.splitext(code_path)[1][1:]]
+        with open(code_path) as file:
+            self.code_path = code_path
+            self.code = file.read()
+        self.messages=[
+            {"role": "system", "content": f'You are an expert in the {language} language.'},
+            {"role": "user", "content": f"Here is some code I want to ask questions about: {self.code}"}]   
 
-def ask_gpt(question, max_tokens = 2000):
-    messages=[
-        {"role": "system", "content": 'You are wise and helpful.'},
-        {"role": "user", "content": question},
-    ]
-    completions = gptchat(messages, max_tokens)
-    return completions
+    def ask(self, question, temperature=0.2):
+        return super().ask(question, temperature=temperature)
+
+    def describe(self, question = CodeInspector.default_question):
+        self.ask(question, temperature=temperature)
